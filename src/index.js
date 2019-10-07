e57f41358ba2f6dc4907cdf8c0aaf5ba410d6c52
oksanatukach <oshabovta@mail.ru>
2019-10-08 00:10:47 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 != 0) {
+    return false;
+  }
+  let result = str;
+    for (let s = 0; s < str.length; s++) {
+      for (let b = 0; b < bracketsConfig.length; b++) {
+        result = result.replace(bracketsConfig[b].join(''), '');
+      }
+  }
+  return result.length === 0;
+};
